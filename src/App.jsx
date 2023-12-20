9bb88f05630f7ded0122b80d446e3bb9ba825a21
Rodrigo <rtf.franca@gmail.com>
2023-12-20 19:26:26 -0300

    Handling possible application errors

--- src/App.jsx
+++ src/App.jsx
@@ -56,17 +56,30 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-      );
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false)
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movies");
+
+        
+        const data = await res.json();
+        if (data.Response === 'False') throw new Error("Movie not found!")
+        setMovies(data.Search);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
@@ -89,7 +102,10 @@ export default function App() {
           }
         /> */}
         <Box>
-         {isLoading ? <Loader /> : <MovieList movies={movies} />}
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
@@ -103,11 +119,19 @@ export default function App() {
 function Loader() {
   return (
     <div>
-      <p className="loader">
-        Loading...
+      <p className="loader">Loading...</p>
+    </div>
+  );
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <div>
+      <p className="error">
+        <span>❌</span> {message}
       </p>
     </div>
-  )
+  );
 }
 
 function Main({ children }) {
