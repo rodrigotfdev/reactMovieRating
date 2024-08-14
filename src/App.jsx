aefe73c6d8f7e7f1f0777dabf257fbb2d72ffe4a
Rodrigo <rtf.franca@gmail.com>
2024-08-14 00:02:51 -0300

    refactor(data-fetching): clean up data fetching code

--- src/App.jsx
+++ src/App.jsx
@@ -84,12 +84,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `https://www.omdbapi.com/?s=${query}&apikey=${apiKEY}`
+            `https://www.omdbapi.com/?s=${query}&apikey=${apiKEY}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -98,8 +101,11 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found!");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AborError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -110,6 +116,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
