54eb482ba8975ac8cd392cdfb55b36dfdf033c70
Rodrigo <rtf.franca@gmail.com>
2024-08-26 22:47:37 -0300

    refactor: move components to separate files

---
+++ src/components/Search.jsx
@@ -0,0 +1,29 @@
+import { useRef, useEffect } from "react";
+
+
+export default function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+  
+    useEffect(function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    }, []);
+  
+    return (
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
+      />
+    );
+  }
