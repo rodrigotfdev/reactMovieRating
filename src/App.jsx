74d0418ec7fadc5655f0e0b8278ceb5f99356707
Rodrigo <rtf.franca@gmail.com>
2024-08-28 13:01:44 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -44,19 +44,19 @@ export default function App() {
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
 
-    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
+    // F.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched]
-  );
+  // useEffect(
+  //   function () {
+  //     localStorage.setItem("watched", JSON.stringify(watched));
+  //   },
+  //   [watched]
+  // );
 
   return (
     <>
