05debe9653cc3018fd46b46ba5a612526b57959a
Rodrigo <rtf.franca@gmail.com>
2024-09-16 15:56:47 -0300

    api url change

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -66,7 +66,7 @@ export default function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${apiKEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${apiKEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
